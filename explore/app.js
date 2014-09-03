1f3cfaf79a4f47de3d0e2b0d187b40ef18381428
Taylor Buley <taylor.buley@gmail.com>
2014-09-03 16:20:11 -0700

    WIP

--- explore/app.js
+++ explore/app.js
@@ -30,14 +30,81 @@ requirejs.config({
 	}
 });
 
-requirejs(['q', 'jquery', 'underscore', 'react', 'dash', 'mapbox'], function(Q, $, _, React, dash, mapbox) {
+requirejs(['q', 'jquery', 'underscore', 'react', 'dash', 'mapbox'], function(Q, $, _, React, dash, L) {
 	console.log("AMD",arguments);
 	var module = Q.defer(),
-		module_promise = module.promise;
+		module_promise = module.promise,
+    doMap = function() {
+      L.mapbox.accessToken = 'pk.eyJ1IjoiYnVsZXkiLCJhIjoiZWwySzE4cyJ9.tKVH4x1b-W4ag-s7jqRKlA';
+      var map = L.mapbox.map('map', 'buley.j737pbkc'),
+          locd= false,
+          getParams = function() {
+            return [ 38.538901, -121.700335, 15 ];
+          },
+          install = function(cb) {
+            addDatabases(function() {
+              addStores( function() {
+                addIndexes(function() {
+                  if ('function' === typeof cb) {
+                    cb();
+                  }
+                } );
+              } );
+            } );
+          },
+          params = getParams(),
+          lat = params[0],
+          lon = params[1],
+          radius = params[2];
+      map.setView( L.latLng(lat, lon), 14 );
+      L.circleMarker( L.latLng(lat, lon), { radius: radius }).addTo(map);
+    },
+    doData = function() {
+
+      dash.get.store({"database": "push", "store": "Places1", "store_key_path": "Id"})(function(ctx) {
+
+          ctx.index = "Name";
+          ctx.index_key_path = "Name";
+          ctx.index_multi_entry = false; //same as default
+          ctx.index_unique = false; //same as default
+
+          dash.get.index(ctx)(function(c) {
+            ctx.index = 'Area';
+            ctx.index_key_path = 'area';
+            dash.get.index(c)(function(z) {
+
+              $.ajax( {
+                method: 'GET',
+                dataType: 'json',
+                url: 'http://23.236.54.41/?latitude=' + lat + '&longitude=' + lon + '&max=1000',
+                success: function(data) {
+                  var x,
+                      xlen = data.Data.length,
+                      item;
+                  for(x = 0; x < xlen; x += 1) {
+                    item = data.Data[x];
+                    var z, zlen = item.Places.length;
+                    for (z = 0; z < zlen; z += 1) {
+                      dash.add.entry({"database": "push", "store": "Places1", "data": item.Places[z]})(function(d) { console.log("Added", d); })
+                    }
+                    L.marker( L.latLng(item.Location.Latitude, item.Location.Longitude)).addTo(map);
+                  }
+                }
+              } );
+            });
+        });
+
+      }, function(d) { console.log("Not Added", d); })
+
+    };
 	module.resolve();
 	React.renderComponent(
 	  React.DOM.div(null, 'Hello, world'),
-	  document.getElementById('explore')
+	  document.getElementById('explore'),
+    function() {
+      doMap();
+      doData();
+    }
 	);
 	return module_promise;
 });
