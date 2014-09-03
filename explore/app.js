b6090289898da2578d747da6cd008adff14e798f
Taylor Buley <taylor.buley@gmail.com>
2014-09-03 16:09:51 -0700

    WIP

--- explore/app.js
+++ explore/app.js
@@ -5,7 +5,8 @@ requirejs.config({
 		'dash': '/bower_components/dash/lib/dash',
 		'mapbox': '/bower_components/mapbox.js/mapbox',
 		'underscore': '/bower_components/underscore/underscore',
-		'jquery': '/bower_components/jquery/dist/jquery.js'
+		'jquery': '/bower_components/jquery/dist/jquery',
+		'react': '/bower_components/react/react'
     },
 	shim: {
 		'Q': {
@@ -22,10 +23,17 @@ requirejs.config({
 		},
 		'jquery': {
 			exports: 'jQuery'
+		},
+		'react': {
+			exports: 'React'
 		}
 	}
 });
 
-requirejs(['q', 'jquery', 'underscore', 'dash', 'mapbox'], function(Q, $, _, dash, mapbox) {
+requirejs(['q', 'jquery', 'underscore', 'react', 'dash', 'mapbox'], function(Q, $, _, dash, mapbox) {
 	console.log("AMD",arguments);
+	var module = Q.defer(),
+		module_promise = module.promise;
+	module.resolve();
+	return module_promise;
 });
