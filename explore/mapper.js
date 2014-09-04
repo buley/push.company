4f8b2396c6371c16fb035a5007b568e3316169c5
Taylor Buley <taylor.buley@gmail.com>
2014-09-03 18:38:08 -0700

    .

--- explore/mapper.js
+++ explore/mapper.js
@@ -7,7 +7,7 @@ define(['q', 'react'], function(Q, React) {
         render: function() {
           return React.DOM.div(null, 'MAYBE');
         }
-      })({});
+      });
   module.resolve(component);
   return {
     outgoing: function(interface) {
