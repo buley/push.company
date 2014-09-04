615f9882bc7bc16d5aef7424285ad1d879f34b6c
Taylor Buley <taylor.buley@gmail.com>
2014-09-03 22:07:58 -0700

    bigquery wip

---
+++ explore/location.js
@@ -0,0 +1,36 @@
+define(['q', 'react'], function(Q, React, L) {
+  var deferred = Q.defer(),
+      promise = deferred.promise,
+      module = Q.defer(),
+      getParams = function() {
+        return [ 38.538901, -121.700335, 15 ];
+      },
+      params = getParams(),
+      lat = params[0],
+      lon = params[1],
+      radius = params[2],
+      map,
+      context = {},
+      component = React.createClass({
+        componentDidMount: function(props) {
+        },
+        render: function() {
+          return React.DOM.div({id: "location"}, JSON.stringify(this.props.location));
+        }
+      });
+  module.resolve(component);
+
+  return {
+    outgoing: function(interface) {
+      interface(promise);
+    },
+    incoming: function(interface) {
+      interface.then(null, null, function(state) {
+        state.location = getParams();
+        deferred.notify(state);
+        context = state;
+      });
+    },
+    ready: module.promise.then.bind(module.promise)
+  };
+});
