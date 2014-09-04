979866d2043ad1a5e906b9f4aa097f14f8ac8e13
Taylor Buley <taylor.buley@gmail.com>
2014-09-03 21:31:27 -0700

    bigquery wip

--- explore/mapper.js
+++ explore/mapper.js
@@ -4,7 +4,7 @@ define(['q', 'react'], function(Q, React) {
       module = Q.defer(),
       context = {},
       render = function() {
-        console.log('props',this.props);
+        console.log('props',this.props, context);
         return React.DOM.div(null, (this.props.init - this.props.timestamp).toString());
       },
       component = React.createClass({
