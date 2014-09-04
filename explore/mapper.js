005389151a2476ceb5f22eb4c66e7f31d24f47b0
Taylor Buley <taylor.buley@gmail.com>
2014-09-03 22:22:19 -0700

    bigquery wip

--- explore/mapper.js
+++ explore/mapper.js
@@ -2,35 +2,24 @@ define(['q', 'react', 'mapbox'], function(Q, React, L) {
   var deferred = Q.defer(),
       promise = deferred.promise,
       module = Q.defer(),
-      getParams = function() {
-        return [ 38.538901, -121.700335, 15 ];
-      },
-      params = getParams(),
-      lat = params[0],
-      lon = params[1],
-      radius = params[2],
       map,
       context = {},
+      marker,
       component = React.createClass({
         componentDidMount: function(props) {
-
             L.mapbox.accessToken = 'pk.eyJ1IjoiYnVsZXkiLCJhIjoiZWwySzE4cyJ9.tKVH4x1b-W4ag-s7jqRKlA';
             map = L.mapbox.map('map', 'buley.j737pbkc');
-            var locd= false;
-                install = function(cb) {
-                  addDatabases(function() {
-                    addStores( function() {
-                      addIndexes(function() {
-                        if ('function' === typeof cb) {
-                          cb();
-                        }
-                      } );
-                    } );
-                  } );
-                },
-            map.setView( L.latLng(lat, lon), 14 );
-            L.circleMarker( L.latLng(lat, lon), { radius: radius }).addTo(map);
-
+        },
+        componentWillReceiveProps: function(props) {
+          var lat = props.location.latitude,
+              long = props.location.longitude;
+          map.setView(L.latLng(lat, lon), 14);
+          if (!marker) {
+            marker = L.circleMarker( L.latLng(lat, lon), { radius: radius }).addTo(map);
+          } else {
+            marker.setLatLng(L.latLng(lat, lon));
+            marker.setRadius({ radius: radius })
+          }
         },
         render: function() {
           return React.DOM.div({id: "mapper"}, (this.props.init - this.props.timestamp).toString());
--- explore/presence.js
+++ explore/presence.js
@@ -3,12 +3,25 @@ define(['q', 'react', 'dash', 'jquery' ], function(Q, React, dash, $) {
       promise = deferred.promise,
       module = Q.defer(),
       context = {},
+      previous,
       render = function() {
         return React.DOM.div(null, (this.props.init - this.props.timestamp).toString());
       },
       component = React.createClass({
+        will
         componentDidMount: function() {
-          var that = this;
+          var that = this,
+              install = function(cb) {
+                addDatabases(function() {
+                  addStores(function() {
+                    addIndexes(function() {
+                      if ('function' === typeof cb) {
+                        cb();
+                      }
+                    } );
+                  } );
+                } );
+              };
           dash.get.store({"database": "push", "store": "Places1", "store_key_path": "Id"})(function(ctx) {
 
               ctx.index = "Name";
@@ -20,25 +33,7 @@ define(['q', 'react', 'dash', 'jquery' ], function(Q, React, dash, $) {
                 ctx.index = 'Area';
                 ctx.index_key_path = 'area';
                 dash.get.index(c)(function(z) {
-
-                  $.ajax( {
-                    method: 'GET',
-                    dataType: 'json',
-                    url: 'http://23.236.54.41/?latitude=' + that.props.location.latitude + '&longitude=' + that.props.location.longitude + '&max=1000',
-                    success: function(data) {
-                      var x,
-                          xlen = data.Data.length,
-                          item;
-                      for(x = 0; x < xlen; x += 1) {
-                        item = data.Data[x];
-                        var z, zlen = item.Places.length;
-                        for (z = 0; z < zlen; z += 1) {
-                          dash.add.entry({"database": "push", "store": "Places1", "data": item.Places[z]})(function(d) { console.log("Added", d); })
-                        }
-                        L.marker( L.latLng(item.Location.Latitude, item.Location.Longitude)).addTo(map);
-                      }
-                    }
-                  } );
+                  console.log('installed');
                 });
             });
 
