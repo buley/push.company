c69f2050c5ff4be30b141fbcc469a7e6a56c1159
Taylor Buley <taylor.buley@gmail.com>
2014-09-03 18:03:47 -0700

    .

--- explore/mapper.js
+++ explore/mapper.js
@@ -1,18 +1,23 @@
 define(['q'], function(Q) {
   var deferred = Q.defer(),
       promise = deferred.promise,
-      module = Q.defer();
+      module = Q.defer(),
+      context;
   module.resolve();
   return {
     outgoing: function(interface) {
-      console.log("mapper.js: outgoing")
       interface(promise);
     },
     incoming: function(interface) {
-      console.log("mapper.js: incoming", arguments)
-      interface.then(null, null, function() {
-        console.log("mapper.js: incoming notification", arguments);
-        deferred.notify("hello from mapper.js");
+      interface.then(null, null, function(state) {
+        state.interfaces = state.interfaces || {};
+        state.interfaces.mapper = {
+          foo: function() {
+            console.log('mapper.js: foo');
+          }
+        };
+        deferred.notify(state);
+        context = state;
       });
     },
     ready: module.promise.then.bind(module.promise)
--- explore/presence.js
+++ explore/presence.js
@@ -1,7 +1,8 @@
 define(['q'], function(Q) {
   var deferred = Q.defer(),
       promise = deferred.promise,
-      module = Q.defer();
+      module = Q.defer(),
+      context;
   module.resolve();
   return {
     outgoing: function(interface) {
@@ -11,8 +12,14 @@ define(['q'], function(Q) {
     incoming: function(interface) {
       console.log("presence.js: incoming", arguments);
       interface.then(null, null, function() {
-        console.log("presence.js: incoming notify", arguments);
-        deferred.notify("hello from presence.js");
+        state.interfaces = state.interfaces || {};
+        state.interfaces.presence = {
+          bar: function() {
+            console.log('presence.js: bar');
+          }
+        };
+        deferred.notify(state);
+        context = state;
       })
     },
     ready: module.promise.then.bind(module.promise)
