cf57d7f40fea4077049a17f02510ab5abf833fc6
Taylor Buley <taylor.buley@gmail.com>
2014-09-05 21:11:56 -0700

    .

--- explore/mapper.js
+++ explore/mapper.js
@@ -6,10 +6,10 @@ define(['q', 'react', 'mapbox'], function(Q, React, L) {
       context = {},
       marker,
       component = React.createClass({
-        componentDidMount: function(props) {
+        componentDidMount: function() {
             L.mapbox.accessToken = 'pk.eyJ1IjoiYnVsZXkiLCJhIjoiZWwySzE4cyJ9.tKVH4x1b-W4ag-s7jqRKlA';
             map = L.mapbox.map('map', 'buley.j737pbkc');
-            marker.setLatLng(L.latLng(props.location.longitude, props.location.longitude));
+            marker.setLatLng(L.latLng(this.props.location.longitude, this.props.location.longitude));
         },
         componentWillReceiveProps: function(props) {
           console.log('props',props);
