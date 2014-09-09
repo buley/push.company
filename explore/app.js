85993b2e41166925056605750466ab4a7774d4ab
Taylor Buley <taylor.buley@gmail.com>
2014-09-09 16:09:51 -0700

    .

--- explore/app.js
+++ explore/app.js
@@ -32,7 +32,7 @@ requirejs.config({
 
 requirejs(['q', 'react'], function(Q, React) {
 	var module = Q.defer();
-    require(['explore/presence', 'explore/mapper', 'explore/location'], function() {
+    require(['explore/presence', 'explore/mapper', 'explore/location', 'explore/urls' ], function() {
       var state = { init: Date.now(), timestamp: Date.now(), location: {
         latitude: 38.538901, longitude: -121.700335, radius: 15 } },
           previous_state = '',
