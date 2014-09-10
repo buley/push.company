94dec4869138e63f22073e227180e49bef3b0f5c
Taylor Buley <taylor.buley@gmail.com>
2014-09-09 19:03:02 -0700

    .

--- explore/presence.js
+++ explore/presence.js
@@ -15,6 +15,7 @@ define(['q', 'react', 'dash', 'jquery', 'underscore', 'explore/trig' ], function
         var distance,
             notify = false,
             prev;
+        console.log("CURRENT",JSON.stringify(current), JSON.stringify(state.location));
         if (!!state.location && JSON.stringify(current) !== JSON.stringify(state.location)) {
           if (!!current) {
             notify = true;
@@ -25,7 +26,7 @@ define(['q', 'react', 'dash', 'jquery', 'underscore', 'explore/trig' ], function
             current = state.location;
             current.arrived = Date.now();
             console.log("MOVED");
-            //deferred.notify(_.extend(state, {location: state.location, previous_location: prev}));
+            deferred.notify(_.extend(state, {location: state.location, previous_location: prev}));
           } else {
             current = state.location;
             current.arrived = Date.now();
