4d5fc71820ad362cde2b1114aaabb68c62e7b1f0
Taylor Buley <taylor.buley@gmail.com>
2014-09-10 15:28:16 -0700

    .

--- explore/presence.js
+++ explore/presence.js
@@ -2,7 +2,22 @@ define(['q', 'react', 'dash', 'jquery', 'underscore', 'explore/trig' ], function
   var deferred = Q.defer(),
       promise = deferred.promise,
       module = Q.defer(),
-      augmented;
+      augmented,
+      fetchNeighbors = function(lat, lon, radius) {
+        console.log('fetching neighbors',arguments);
+        $.ajax({
+          url: [
+            'http://23.236.54.41/presence?',
+            'latitude=' + lat + '&',
+            'longitude='+ lon + '&',
+            'max=1000&limit=10&radius=' + radius
+            ].join(""),
+          method: 'GET',
+          success: function(data) {
+            console.log('data',data);
+          }
+        })
+      };
   module.resolve();
 
   return {
