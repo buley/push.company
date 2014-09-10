1d89776bb3183f1c93352aa4742ec670093a13fa
Taylor Buley <taylor.buley@gmail.com>
2014-09-09 19:24:17 -0700

    .

--- explore/app.js
+++ explore/app.js
@@ -81,7 +81,7 @@ requirejs(['q', 'react'], function(Q, React) {
               component.replaceProps(ctx);
               if (next_state !== previous_state) {
                 previous_state = next_state;
-                console.log("NOTIFY STATE");
+                console.log("NOTIFY STATE", next_state, previous_state);
                 //deferred.notify(ctx);
               }
             }
