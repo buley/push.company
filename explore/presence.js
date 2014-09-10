95552e77f2f0004d52e472ea8c78121d9a67209f
Taylor Buley <taylor.buley@gmail.com>
2014-09-09 20:22:57 -0700

    .

--- explore/presence.js
+++ explore/presence.js
@@ -18,16 +18,11 @@ define(['q', 'react', 'dash', 'jquery', 'underscore', 'explore/trig' ], function
           if (!!augmented) {
             if (augmented.latitude !== state.location.latitude ||
               augmented.longitude !== state.location.longitude) {
-              console.log('CURRENT',augmented.latitude, augmented.longitude);
-              console.log('STATE',state.location.latitude, state.location.longitude);
-              if (augmented.latitude !== state.location.latitude ||
-                augmented.longitude !== state.location.longitude) {
-                  distance = trig.distance(augmented, state.location);
-                  augmented.duration = Date.now() - augmented.arrived;
-                  augmented.distance = Infinity === distance ? null : distance;
-                  context = _.extend(state, {location: state.location, previous_location: augmented});
-                  deferred.notify(context);
-              }
+              distance = trig.distance(augmented, state.location);
+              augmented.duration = Date.now() - augmented.arrived;
+              augmented.distance = Infinity === distance ? null : distance;
+              context = _.extend(state, {location: state.location, previous_location: augmented});
+              deferred.notify(context);
               augmented = {
                   latitude: state.location.latitude,
                   longitude: state.location.longitude,
