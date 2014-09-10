eb19fc0407fee722e6bfc511b9ff8a89bd93a8b2
Taylor Buley <taylor.buley@gmail.com>
2014-09-09 18:37:46 -0700

    .

--- explore/presence.js
+++ explore/presence.js
@@ -3,44 +3,7 @@ define(['q', 'react', 'dash', 'jquery', 'underscore', 'explore/trig' ], function
       promise = deferred.promise,
       module = Q.defer(),
       context = {},
-      current,
-      previous,
-      render = function() {
-        return React.DOM.div(null, (this.props.init - this.props.timestamp).toString());
-      },
-      component = React.createClass({
-        componentDidMount: function() {
-          var that = this,
-              install = function(cb) {
-                addDatabases(function() {
-                  addStores(function() {
-                    addIndexes(function() {
-                      if ('function' === typeof cb) {
-                        cb();
-                      }
-                    } );
-                  } );
-                } );
-              };
-
-          dash.get.store({"database": "push", "store": "Places1", "store_key_path": "Id"})(function(ctx) {
-            ctx.index = "Name";
-            ctx.index_key_path = "Name";
-            ctx.index_multi_entry = false; //same as default
-            ctx.index_unique = false; //same as default
-            dash.get.index(ctx)(function(c) {
-              ctx.index = 'Area';
-              ctx.index_key_path = 'area';
-              dash.get.index(c)(function(z) {
-                console.log('installed');
-              });
-            });
-          }, function(d) { console.log("Not Added", d); } );
-
-        },
-        render: render
-      });
-
+      current = {};
   module.resolve();
 
   return {
