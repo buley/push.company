8a571d2dc59969054c028aa3af6096cfc30cc208
Taylor Buley <taylor.buley@gmail.com>
2014-09-10 20:48:45 -0700

    .

--- explore/mapper.js
+++ explore/mapper.js
@@ -18,107 +18,111 @@ define(['q', 'react', 'mapbox', 'underscore'], function(Q, React, L, _) {
             map.on('overlayremove', onOverlayRemove);
         },
         render: function() {
-          var key,
-              layer,
-              ids = [];
-          if (!marker && !!this.props && !!this.props.location) {
-            marker = L.circleMarker( [this.props.location.latitude, this.props.location.longitude], {
-              color: '#000',
-              fill: false,
-              radius: this.props.location.radius
-            } );
-            marker.addTo(map);
-          } else if (!!marker && !!this.props && !!this.props.location) {
-            marker.setLatLng([this.props.location.latitude, this.props.location.longitude]);
-            marker.setRadius(this.props.location.radius);
-          }
-          if (!!this.props.vicinity) {
-            key = [
-                this.props.location.latitude,
-                this.props.location.longitude,
-                this.props.location.radius,
-              ].join("|");
-            if (!!vicinities[key]) {
-              map.removeLayer(vicinities[key].group);
-              delete vicinities[key];
-            }
-            layer = { data: [] };
-            if (!!this.props.vicinity && this.props.vicinity.length > 0) {
-              this.props.vicinity.forEach(function(item) {
-                if (!!item.Places && item.Places.length > 0) {
-                  item.Places.forEach(function(place) {
-                    ids.push(item.Id);
-                    layer.data.push(L.marker([item.Location.Latitude, item.Location.Longitude]).bindPopup(place.Name));
-                  });
-                }
-              });
-            }
-            if (layer.data.length > 0) {
-              layer.group = L.featureGroup(layer.data);
-              layer.group.addTo(map);
-              control_layers = control_layers || {};
-              control_layers["Hyperlocal"] = layer.group;
-              vicinities[key] = layer;
-            }
-          }
-          if (!!this.props.neighborhood) {
-            key = [
-                this.props.location.latitude,
-                this.props.location.longitude,
-                this.props.location.radius,
-              ].join("|");
-            if (!!layers[key]) {
-              map.removeLayer(layers[key].group);
-              delete layers[key];
-            }
-            layer = { data: ( !!layer ? layer.data || [] : [] ) };
-            if (!!this.props.neighborhood && this.props.neighborhood.length > 0) {
-              this.props.neighborhood.forEach(function(place) {
-                if (false === _.contains(overlays, "Hyperlocal") || false === _.contains(ids, place.Id)) {
-                  layer.data.push(
-                    L.marker([place.Latitude, place.Longitude]).bindPopup(place.Name)
-                  );
-                }
-              });
-            }
-            if (layer.data.length > 0) {
-              layer.group = L.featureGroup(layer.data);
-              map.fitBounds(layer.group.getBounds());
-              layer.group.addTo(map);
-              layers[key] = layer;
-              control_layers = control_layers || {};
-              control_layers["Local"] = layer.group;
-            }
-          }
-
-          if (!!this.props.previous_location) {
-            key = [
-                this.props.previous_location.latitude,
-                this.props.previous_location.longitude,
-                this.props.previous_location.radius,
-              ].join("|");
-            if (!!vicinities[key]) {
-              map.removeLayer(vicinities[key].group);
-              delete vicinities[key];
-            }
-            if (!!layers[key]) {
-              map.removeLayer(layers[key].group);
-              delete layers[key];
-            }
-          }
-          if (!!map && !control && !!control_layers) {
-            console.log('CONTROL',control_layers);
-            control = L.control.layers(control_layers)
-            control.addTo(map);
-          } else if (!!map && !!control_layers && !!control) {
-            control.removeFrom(map);
-            control = L.control.layers({}, control_layers)
-            control.addTo(map);
-          }
-
+          renderMap();
           return React.DOM.div({id: "map"});
         }
       }),
+      renderMap = function() {
+
+        var key,
+            layer,
+            ids = [];
+        if (!marker && !!this.props && !!this.props.location) {
+          marker = L.circleMarker( [this.props.location.latitude, this.props.location.longitude], {
+            color: '#000',
+            fill: false,
+            radius: this.props.location.radius
+          } );
+          marker.addTo(map);
+        } else if (!!marker && !!this.props && !!this.props.location) {
+          marker.setLatLng([this.props.location.latitude, this.props.location.longitude]);
+          marker.setRadius(this.props.location.radius);
+        }
+        if (!!this.props.vicinity) {
+          key = [
+              this.props.location.latitude,
+              this.props.location.longitude,
+              this.props.location.radius,
+            ].join("|");
+          if (!!vicinities[key]) {
+            map.removeLayer(vicinities[key].group);
+            delete vicinities[key];
+          }
+          layer = { data: [] };
+          if (!!this.props.vicinity && this.props.vicinity.length > 0) {
+            this.props.vicinity.forEach(function(item) {
+              if (!!item.Places && item.Places.length > 0) {
+                item.Places.forEach(function(place) {
+                  ids.push(item.Id);
+                  layer.data.push(L.marker([item.Location.Latitude, item.Location.Longitude]).bindPopup(place.Name));
+                });
+              }
+            });
+          }
+          if (layer.data.length > 0) {
+            layer.group = L.featureGroup(layer.data);
+            layer.group.addTo(map);
+            control_layers = control_layers || {};
+            control_layers["Hyperlocal"] = layer.group;
+            vicinities[key] = layer;
+          }
+        }
+        if (!!this.props.neighborhood) {
+          key = [
+              this.props.location.latitude,
+              this.props.location.longitude,
+              this.props.location.radius,
+            ].join("|");
+          if (!!layers[key]) {
+            map.removeLayer(layers[key].group);
+            delete layers[key];
+          }
+          layer = { data: ( !!layer ? layer.data || [] : [] ) };
+          if (!!this.props.neighborhood && this.props.neighborhood.length > 0) {
+            this.props.neighborhood.forEach(function(place) {
+              if (false === _.contains(overlays, "Hyperlocal") || false === _.contains(ids, place.Id)) {
+                layer.data.push(
+                  L.marker([place.Latitude, place.Longitude]).bindPopup(place.Name)
+                );
+              }
+            });
+          }
+          if (layer.data.length > 0) {
+            layer.group = L.featureGroup(layer.data);
+            map.fitBounds(layer.group.getBounds());
+            layer.group.addTo(map);
+            layers[key] = layer;
+            control_layers = control_layers || {};
+            control_layers["Local"] = layer.group;
+          }
+        }
+
+        if (!!this.props.previous_location) {
+          key = [
+              this.props.previous_location.latitude,
+              this.props.previous_location.longitude,
+              this.props.previous_location.radius,
+            ].join("|");
+          if (!!vicinities[key]) {
+            map.removeLayer(vicinities[key].group);
+            delete vicinities[key];
+          }
+          if (!!layers[key]) {
+            map.removeLayer(layers[key].group);
+            delete layers[key];
+          }
+        }
+        if (!!map && !control && !!control_layers) {
+          console.log('CONTROL',control_layers);
+          control = L.control.layers(control_layers)
+          control.addTo(map);
+        } else if (!!map && !!control_layers && !!control) {
+          control.removeFrom(map);
+          control = L.control.layers({}, control_layers)
+          control.addTo(map);
+        }
+
+      },
       onBaseLayerChange = function(e) {
         console.log('onBaseLayerChange',e);
       },
@@ -131,7 +135,7 @@ define(['q', 'react', 'mapbox', 'underscore'], function(Q, React, L, _) {
         overlays = _.unique(overlays);
         console.log('onOverlayAdd',overlays);
       };
-      
+
   module.resolve(component);
 
   L.mapbox.accessToken = 'pk.eyJ1IjoiYnVsZXkiLCJhIjoiZWwySzE4cyJ9.tKVH4x1b-W4ag-s7jqRKlA';
