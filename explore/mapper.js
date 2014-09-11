111e96bfcec213378ffe596d2112cc56b0204bd0
Taylor Buley <taylor.buley@gmail.com>
2014-09-10 19:38:10 -0700

    .

--- explore/mapper.js
+++ explore/mapper.js
@@ -100,7 +100,7 @@ define(['q', 'react', 'mapbox'], function(Q, React, L) {
             }
           }
 
-          L.control.layers(null, control_layers).addTo(map);
+          L.control.layers({}, control_layers).addTo(map);
 
           return React.DOM.div({id: "map"});
         }
