6e36d32675c8b9dfe2734a5be078c6cd4d8fc647
Taylor Buley <taylor.buley@gmail.com>
2014-09-10 21:29:22 -0700

    .

--- explore/mapper.js
+++ explore/mapper.js
@@ -10,7 +10,7 @@ define(['q', 'react', 'mapbox', 'underscore'], function(Q, React, L, _) {
       vicinities = {},
       marker,
       control,
-      overlays = [ "Hyperlocal" ],
+      overlays = [ "Local" ],
       instance,
       component = React.createClass({
         componentDidMount: function() {
