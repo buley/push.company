c3f6d4891ff2344d6be7bb651d962ec34ad1870f
Taylor Buley <taylor.buley@gmail.com>
2014-09-10 21:06:15 -0700

    .

--- explore/mapper.js
+++ explore/mapper.js
@@ -133,14 +133,14 @@ define(['q', 'react', 'mapbox', 'underscore'], function(Q, React, L, _) {
       },
       onOverlayRemove = function(e) {
         overlays = _.without(overlays, e.name);
-        console.log('onOverlayRemove',overlays);
-        renderMap.apply(instance, []);
+        console.log('onOverlayRemove',e.name, overlays);
+        //renderMap.apply(instance, []);
       },
       onOverlayAdd = function(e) {
         overlays.push(e.name);
         overlays = _.unique(overlays);
-        console.log('onOverlayAdd',overlays);
-        renderMap.apply(instance, []);
+        console.log('onOverlayAdd',e.name, overlays);
+        //renderMap.apply(instance, []);
       };
 
   module.resolve(component);
