e9adb58d42c3a34dc7bac4b16b3f03c89d0ae96c
Taylor Buley <taylor.buley@gmail.com>
2014-09-22 14:15:17 -0700

    .

---
+++ src/header.js
@@ -0,0 +1,38 @@
+define(['q', 'react', 'mapbox', 'underscore'], function(Q, React, L, _) {
+  var deferred = Q.defer(),
+      promise = deferred.promise,
+      module = Q.defer(),
+      context = {},
+      instance,
+      component = React.createClass({
+        componentDidMount: function() {
+            instance = this;
+            console.log("header mounted");
+        },
+        render: function() {
+          //TODO: Render map
+          //renderMap.apply(this, arguments);
+          return React.DOM.div({id: "header"});
+        }
+      });
+
+  module.resolve(component);
+
+  return {
+    outgoing: function(interface) {
+      interface(promise);
+    },
+    incoming: function(interface) {
+      interface.then(null, null, function(state) {
+        context = _.extend({}, state);
+        if (!!state.route && !!state.route.hash && !!state.route.hash.overlay) {
+          overlays = _.unique(state.route.hash.overlay.split(","));
+          map_state.overlays = overlays;
+          delete context.route.hash.overlay;
+          deferred.notify(context);
+        }
+      });
+    },
+    ready: module.promise.then.bind(module.promise)
+  };
+});
