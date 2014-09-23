c9d8145964b1eb26ece0a28934acdb89c15d0dc4
Taylor Buley <taylor.buley@gmail.com>
2014-09-23 14:13:03 -0700

    .

---
+++ src/content.js
@@ -0,0 +1,84 @@
+define(['q', 'react', 'underscore'], function(Q, React, _) {
+  var deferred = Q.defer(),
+      promise = deferred.promise,
+      module = Q.defer(),
+      context,
+      instance,
+      prev = {},
+      padding = {
+        top: 20,
+        bottom: 20,
+        left: 20,
+        right: 20
+      },
+      onResize = _.debounce(function() {
+        console.log('resize content');
+      }),
+      nodeHeight = function(el, sum) {
+        if (!el) {
+          return NaN;
+        }
+        var x, xlen;
+        sum = sum || 0;
+        if (!!el.children && el.children.length > 0) {
+          for (x = 0, xlen = el.children.length; x < xlen; x += 1) {
+            sum += nodeHeight(el.children[x], 0);
+          }
+        }
+        sum += el.offsetHeight;
+        return sum;
+      },
+      nodeWidth = function(el, sum) {
+        if (!el) {
+          return NaN;
+        }
+        var x, xlen;
+        sum = sum || 0;
+        if (!!el.children && el.children.length > 0) {
+          for (x = 0, xlen = el.children.length; x < xlen; x += 1) {
+            sum += nodeWidth(el.children[x], 0);
+          }
+        }
+        sum += el.offsetWidth;
+        return sum;
+      },
+      component = React.createClass({
+        componentDidMount: function() {
+            instance = this;
+            console.log('content did mount')
+        },
+        render: function() {
+
+          return React.DOM.section({
+            id: "content"
+          } );
+        }
+      });
+
+  module.resolve(component);
+
+
+  return {
+    outgoing: function(interface) {
+      interface(promise);
+    },
+    incoming: function(interface) {
+      interface.then(null, null, function(state) {
+        if (!context) {
+          context = _.extend({}, _.extend({ ads: { loaded: false }}, state));
+          deferred.notify(context);
+        } else {
+          context = _.extend({}, state);
+          if (context.screen) {
+            if (context.screen.width !== prev.width || context.screen.height !== prev.height) {
+              onResize(context.screen.width, context.screen.height, prev.width, prev.height);
+              prev.width = context.screen.width;
+              prev.height = context.screen.height;
+            }
+          }
+        }
+      });
+    },
+    ready: module.promise.then.bind(module.promise)
+  };
+});
