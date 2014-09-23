14a5cfd97f12441fdae535e2fdd3349a14de940a
Taylor Buley <taylor.buley@gmail.com>
2014-09-23 13:51:39 -0700

    .

--- src/ads.js
+++ src/ads.js
@@ -125,6 +125,7 @@ define(['q', 'react', 'underscore'], function(Q, React, _) {
             id: "ads-banner-bottom",
             style: {
               "left": top_width_base + "px",
+              "top": (this.props.header && this.props.header.height ? this.props.header.height + top_height_base + top_height: top_height_base) + "px",
               "bottom": bottom_height_base + "px"
             }
           }, React.DOM.div({
