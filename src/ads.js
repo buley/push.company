7221077affcbe7d3caf78197d11edeaecb7313c9
Taylor Buley <taylor.buley@gmail.com>
2014-09-23 13:57:45 -0700

    .

--- src/ads.js
+++ src/ads.js
@@ -140,7 +140,7 @@ define(['q', 'react', 'underscore'], function(Q, React, _) {
     if (!_.contains(slots, event.slot)) {
       slots.push(event.slot);
     }
-    if (expecting && ++seen >= expecting) {
+    if (expecting > 0 && ++seen >= expecting) {
       if (instance && instance.isMounted()) {
         var node = instance.getDOMNode(),
             width = node.clientWidth,
