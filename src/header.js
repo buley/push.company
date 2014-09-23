c3bdcb2ec10f17bdb2afeddc489b203eb75504c4
Taylor Buley <taylor.buley@gmail.com>
2014-09-22 21:54:00 -0700

    .

--- src/header.js
+++ src/header.js
@@ -85,7 +85,7 @@ define(['q', 'react', 'mapbox', 'underscore', 'tween'], function(Q, React, L, _,
               animate();
 
               var tween = new Tween.Tween( { height: instance.props.header && instance.props.header.drawer && instance.props.header.drawer.showing ? 200 : 0 } )
-                .to( { height: instance.props.header && instance.props.header.drawer && instance.props.header.drawer.showing ? 0 : 200 }, 300 )
+                .to( { height: instance.props.header && instance.props.header.drawer && instance.props.header.drawer.showing ? 0 : 200 }, instance.props.header && instance.props.header.drawer && instance.props.header.drawer.showing ? 300 : 1000 )
                 .easing( Tween.Easing.Elastic.InOut )
                 .onUpdate(function() {
                     context.header.drawer = _.extend(context.header.drawer, {
