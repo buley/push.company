8a1b09192ab6b2e90192715e073ade7ff25ac56b
Taylor Buley <taylor.buley@gmail.com>
2014-09-23 16:49:13 -0700

    .

--- src/ads.js
+++ src/ads.js
@@ -142,70 +142,39 @@ define(['q', 'react', 'underscore'], function(Q, React, _) {
         slots.push(event.slot);
       }
     }
-    if (expecting > 0 && ++seen >= expecting) {
-      console.log('slots finished');
-      if (instance && instance.isMounted()) {
 
-        var mounted = this.isMounted(),
-            top_el = document.getElementById("ads-banner-top") || {},
-            top_height = mounted && top_el.offsetHeight ? top_el.offsetHeight : 0,
-            top_width = mounted && top_el.offsetWidth ? top_el.offsetWidth : 0,
-            bottom_el = document.getElementById("ads-banner-bottom") || {},
-            bottom_height = mounted && bottom_el.offsetHeight ? bottom_el.offsetHeight : 0,
-            bottom_width = mounted && bottom_el.offsetWidth ? bottom_el.offsetWidth : 0,
-            total_width = mounted && this.props.screen ? this.props.screen.width : 0,
-            total_width_padding = total_width - top_width,
-            total_width_padding_bottom = total_width - bottom_width,
-            top_height_base = Math.floor((padding.top + padding.bottom)/2),
-            top_width_base = Math.floor((padding.right + padding.left + total_width_padding)/2),
-            bottom_width_base = Math.floor((padding.right + padding.left + total_width_padding_bottom)/2),
-            bottom_height_base = 20;
+    if (instance && instance.isMounted()) {
 
-        context.ads.sizes = _.extend((context.ads.sizes || {}), {
-          "banner-top": {
-            height: top_height,
-            width: top_width
-          },
-          "banner-bottom": {
-            height: bottom_height,
-            width: bottom_width
-          }
-        });
+      var mounted = this.isMounted(),
+          top_el = document.getElementById("ads-banner-top") || {},
+          top_height = mounted && top_el.offsetHeight ? top_el.offsetHeight : 0,
+          top_width = mounted && top_el.offsetWidth ? top_el.offsetWidth : 0,
+          bottom_el = document.getElementById("ads-banner-bottom") || {},
+          bottom_height = mounted && bottom_el.offsetHeight ? bottom_el.offsetHeight : 0,
+          bottom_width = mounted && bottom_el.offsetWidth ? bottom_el.offsetWidth : 0,
+          total_width = mounted && this.props.screen ? this.props.screen.width : 0,
+          total_width_padding = total_width - top_width,
+          total_width_padding_bottom = total_width - bottom_width,
+          top_height_base = Math.floor((padding.top + padding.bottom)/2),
+          top_width_base = Math.floor((padding.right + padding.left + total_width_padding)/2),
+          bottom_width_base = Math.floor((padding.right + padding.left + total_width_padding_bottom)/2),
+          bottom_height_base = 20;
 
-        seen = 0;
-        expecting = 0;
-        first = false;
-
-        deferred.notify(context);
-
-        /*
-        var node = document.getElementById("ads-banner-top-ad"),
-            width = node.clientWidth,
-            height = node.clientHeight,
-            notify = false;
-
-        sizes["top-banner"] = sizes["top-banner"] || {};
-        if ( sizes["top-banner"].width !== width) {
-          notify = true;
-          sizes["top-banner"].width = width;
+      context.ads.sizes = _.extend((context.ads.sizes || {}), {
+        "banner-top": {
+          height: top_height,
+          width: top_width
+        },
+        "banner-bottom": {
+          height: bottom_height,
+          width: bottom_width
         }
-        if ( sizes["top-banner"].height !== height) {
-          notify = true;
-          sizes["top-banner"].height = height;
-        }
-        if (true === notify) {
-          seen = 0;
-          expecting = 0;
-          context.ads = context.ads || {};
-          context.ads.sizes = _.extend((context.ads.sizes || {}), sizes);
-          console.log('notify sizes',sizes);
-          deferred.notify(context);
-          if (false === first) {
-            first = true;
-          }
-        }*/
+      });
 
-      }
+      deferred.notify(context);
+
+      first = false;
+
     }
 
   });
