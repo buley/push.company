4c7301e899bb2691551555a35d4a99e1342809b6
Taylor Buley <taylor.buley@gmail.com>
2014-09-22 18:24:09 -0700

    .

--- src/header.js
+++ src/header.js
@@ -15,7 +15,7 @@ define(['q', 'react', 'mapbox', 'underscore'], function(Q, React, L, _) {
             sum += nodeHeight(el.children[x], 0);
           }
         }
-        sum += el.offsetHeight;
+        sum += el.offsetHeight + (parseFloat(el.style.getPropertyValue('margin-top')) || 0) + (parseFloat(el.style.getPropertyValue('margin-bottom')) || 0);
         return sum;
       },
       nodeWidth = function(el, sum) {
