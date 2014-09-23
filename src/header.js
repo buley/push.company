cb027cb8b5d981b3429ad89c96b839439bc43140
Taylor Buley <taylor.buley@gmail.com>
2014-09-22 20:17:02 -0700

    .

--- src/header.js
+++ src/header.js
@@ -84,13 +84,13 @@ define(['q', 'react', 'mapbox', 'underscore'], function(Q, React, L, _) {
           })))), React.DOM.div({
             id: "header-under",
             style: {
-              top: (this.props.header ? this.props.header.height : 0) + "px",
               height: ( this.props.header ? this.props.header.height : 0 ) + "px"
             }
           }), React.DOM.div({
             id: "header-drawer",
             style: {
-              height: '200px'
+              top: (this.props.header ? this.props.header.height : 0) + "px",
+              height: ( this.props.header ? this.props.header.height : 0 ) + "px"
             }
           }));
         }
