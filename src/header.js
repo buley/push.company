9b08239d422f9e5d9c71a7868ee0876c3c6bf77a
Taylor Buley <taylor.buley@gmail.com>
2014-09-22 19:39:10 -0700

    .

--- src/header.js
+++ src/header.js
@@ -74,7 +74,12 @@ define(['q', 'react', 'mapbox', 'underscore'], function(Q, React, L, _) {
             }
           }, React.DOM.div({
             id: "header"
-          })), React.DOM.div({
+          }, React.DOM.div({
+            id: "header-menu-button",
+            style: {
+              background: "#FFF"
+            }
+          }))), React.DOM.div({
             id: "header-under",
             style: {
               height: ( this.props.header ? this.props.header.height : 0 ) + "px"
