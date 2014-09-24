4b6b59c7d069931c80927adf9b2012bb90ffc0b2
Taylor Buley <taylor.buley@gmail.com>
2014-09-23 18:20:46 -0700

    .

--- src/ads.js
+++ src/ads.js
@@ -54,6 +54,7 @@ define(['q', 'react', 'underscore'], function(Q, React, _) {
         expect = slots.length;
         seen = 0;
         window.googletag.pubads().refresh(slots);
+        adjustAds();
       }, 3000 ),
       usable = [],
       first = false,
