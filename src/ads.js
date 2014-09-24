ed85bc048a2966c8a2ef48b0def08d052d89e102
Taylor Buley <taylor.buley@gmail.com>
2014-09-24 15:10:07 -0700

    .

--- src/ads.js
+++ src/ads.js
@@ -272,12 +272,8 @@ define(['q', 'react', 'underscore'], function(Q, React, _) {
     }
 
     if (expecting > 0 && ++seen >= expecting) {
-      console.log("Finished processing",expecting);
       seen = 0;
       expecting = 0;
-      adjustAds(context);
-    } else {
-      console.log("Processing", expecting, seen);
       adjustAds(context);
     }
 
