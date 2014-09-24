7f96246283f35d3eeddcec91f19ad3e5d176d895
Taylor Buley <taylor.buley@gmail.com>
2014-09-24 14:33:19 -0700

    .

--- src/header.js
+++ src/header.js
@@ -189,13 +189,13 @@ define(['q', 'react', 'mapbox', 'underscore', 'tween'], function(Q, React, L, _,
           context = _.extend({}, state);
 
           context.header = context.header || {};
-          context.header = _.extend(context.header, {header: { height: 44, zoom: mult, drawer: {
+          context.header = _.extend(context.header, { height: 44, zoom: mult, drawer: {
             showing: false,
             height: 0,
             selected: "categories"
           }, logo: {
             text: "Lipsum Daily"
-          } } } );
+          } } );
           deferred.notify(context);
 
         } else {
