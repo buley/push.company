b052b47f08b3c13a526cabeeefa59f582b3d5dfa
Taylor Buley <taylor.buley@gmail.com>
2014-09-23 18:55:38 -0700

    .

--- src/content.js
+++ src/content.js
@@ -23,7 +23,7 @@ define(['q', 'react', 'underscore'], function(Q, React, _) {
           return React.DOM.section({
             id: "content-container",
             style: {
-              top: "200px"
+              top: this.props.header && this.props.header.height ? this.props.header.height + this.props.ads['banner-top'].height: 0
             }
           }, React.DOM.section({
             id: "content"
