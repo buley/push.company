ec6afaee43ca4d05b6ca1b5ce8926b8e84532766
Taylor Buley <taylor.buley@gmail.com>
2014-09-24 15:11:36 -0700

    .

--- src/app.js
+++ src/app.js
@@ -76,7 +76,7 @@ requirejs.config({
 
 requirejs(['q', 'react', 'underscore'], function(Q, React, _) {
 	var module = Q.defer();
-    require(['src/ads', 'src/content', 'src/header', 'src/urls', 'src/footer', 'src/sidebar' ], function() {
+    require(['src/ads', 'src/header', 'src/content', 'src/sidebar', 'src/footer',  'src/urls'], function() {
       var state = {
             screen: {
               width: document.body.clientWidth,
