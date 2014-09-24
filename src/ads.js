ec076406632f54f53919c9e3a500212e8a09ee49
Taylor Buley <taylor.buley@gmail.com>
2014-09-24 16:12:55 -0700

    .

--- src/ads.js
+++ src/ads.js
@@ -274,7 +274,6 @@ define(['q', 'react', 'underscore'], function(Q, React, _) {
       interface.then(null, null, function(state) {
         if (!context) {
           context = _.extend({}, state);
-          adjustAds(context);
         } else {
           var refresh = false,
               adjust = false;
