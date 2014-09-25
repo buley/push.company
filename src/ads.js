fa9218650d899d472f8db7707dc26d638f4918a9
Taylor Buley <taylor.buley@gmail.com>
2014-09-24 18:55:32 -0700

    .

--- src/ads.js
+++ src/ads.js
@@ -233,10 +233,10 @@ define(['q', 'react', 'underscore'], function(Q, React, _) {
     top_box.defineSizeMapping(boxmapping);
     slots.push(top_box);
 
-    /*bottom_box = googletag.defineSlot('/270461283/Box', [[88, 31]], "ads-box-bottom-ad")
+    bottom_box = googletag.defineSlot('/270461283/Box', [[88, 31]], "ads-box-bottom-ad")
         .addService(googletag.pubads());
     slots.push(bottom_box);
-    bottom_box.defineSizeMapping(boxmapping);*/
+    bottom_box.defineSizeMapping(boxmapping);
 
 
     expecting = slots.length;
