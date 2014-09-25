b0593fdfeb8874217cfa2047409baae6168ee647
Taylor Buley <taylor.buley@gmail.com>
2014-09-24 23:40:33 -0700

    .

--- src/content.js
+++ src/content.js
@@ -26,7 +26,7 @@ define(['q', 'react', 'underscore'], function(Q, React, _) {
             instance = this;
         },
         render: function() {
-          console.log(this.props.ads ? this.props.ads['box-top'].top - (this.props.ads['box-bottom'].top + this.props.ads['box-bottom'].height) : 'xxx');
+          var sidebar_height = this.props.ads ? (this.props.ads['box-top'].top - 20) - (this.props.ads['box-bottom'].top + this.props.ads['box-bottom'].height + 20) : 0;
           return React.DOM.section({
             id: "content-container",
             style: {
