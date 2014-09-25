9d1298d3eb26a5b9994ea12784acfc494721c950
Taylor Buley <taylor.buley@gmail.com>
2014-09-24 20:11:57 -0700

    .

--- src/app.js
+++ src/app.js
@@ -197,8 +197,8 @@ requirejs(['q', 'react', 'underscore'], function(Q, React, _) {
 
       requestStateChange(_.extend(context, { init: Date.now() }));
 
-      window.addEventListener("resize", _.debounce( onResize ) );
-      window.addEventListener("scroll", _.debounce( onScroll ) );
+      window.addEventListener("resize", onResize);
+      window.addEventListener("scroll", onScroll);
 
     });
 	return module.promise;
