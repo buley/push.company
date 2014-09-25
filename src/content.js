71931ff39217e19f1a0a9dfdd40181ddb37359d7
Taylor Buley <taylor.buley@gmail.com>
2014-09-25 14:50:20 -0700

    .

--- src/content.js
+++ src/content.js
@@ -37,16 +37,15 @@ define(['q', 'react', 'underscore'], function(Q, React, _) {
               box_top_el = document.getElementById("ads-box-top") || {},
               box_top_height = box_top_el.offsetHeight ? box_top_el.offsetHeight : 0,
               box_top_width = box_top_el.offsetWidth ? box_top_el.offsetWidth : 0,
-              sidebar = box_top_height + box_bottom_height, //plus stream TK
               top_el = document.getElementById("ads-banner-top") || {},
               top_height = top_el.offsetHeight ? top_el.offsetHeight : 0,
               top_width = top_el.offsetWidth ? top_el.offsetWidth : 0,
-              height = 800,
-              content_top = header_height + top_height,
-              sidebar = box_top_height + box_bottom_height,
               bottom_el = document.getElementById("ads-banner-bottom") || {},
               bottom_height = bottom_el.offsetHeight ? bottom_el.offsetHeight : 0,
-              bottom_width = bottom_el.offsetWidth ? bottom_el.offsetWidth : 0;
+              bottom_width = bottom_el.offsetWidth ? bottom_el.offsetWidth : 0,
+              height = 800,
+              sidebar = box_top_height + box_bottom_height, //plus stream TK
+              content_top = header_height + top_height;
 
           if (sidebar > height) {
             height = sidebar;
