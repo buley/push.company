0f028a85c700c9a293643c4d698335cf505c7962
Taylor Buley <taylor.buley@gmail.com>
2014-09-27 00:01:03 -0700

    .

--- src/left.js
+++ src/left.js
@@ -2,31 +2,7 @@ define(['q', 'react', 'underscore', 'src/layout'], function(Q, React, _, layout)
   var deferred = Q.defer(),
       promise = deferred.promise,
       module = Q.defer(),
-      context,
-      first = true,
-      instance,
-      prev = {},
-      left,
-      padding = {
-        top: 20,
-        bottom: 20,
-        left: 20,
-        left: 20
-      },
-      onResize = function(ctx) {
-        return;
-        var el = document.getElementById("left-container"),
-            style = window.getComputedStyle(el);
-        ctx.left = {
-          height: parseFloat(el.height.replace(/px$/, ''),10),
-          top: parseFloat(el.top.replace(/px$/, ''),10)
-        };
-        deferred.notify(ctx);
-      },
       component = React.createClass({
-        componentDidMount: function() {
-            instance = this;
-        },
         render: function() {
           var current = layout.current();
           return React.DOM.section({
@@ -47,34 +23,6 @@ define(['q', 'react', 'underscore', 'src/layout'], function(Q, React, _, layout)
   return {
     outgoing: function(interface) {
       interface(promise);
-    },
-    incoming: function(interface) {
-      interface.then(null, null, function(state) {
-        var json;
-        if (first) {
-          first = false;
-          if (state.screen) {
-            prev.updated = state.screen.updated;
-          }
-          onResize(state);
-        } else {
-          if (!state.left) {
-            state.left = left;
-            deferred.notify(state);
-          } else if (state.screen) {
-            if (state.screen.updated !== prev.updated) {
-              prev.updated = state.screen.updated;
-              onResize(state);
-            }
-          } else {
-            json = JSON.stringify(state.ads);
-            if (json !== prev.json) {
-              prev.json = json;
-              onResize(state);
-            }
-          }
-        }
-      });
     },
     ready: module.promise.then.bind(module.promise)
   };
