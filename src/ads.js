60662578781d417c21eef2dc01cd9f27f480969c
Taylor Buley <taylor.buley@gmail.com>
2014-09-26 21:02:37 -0700

    .

--- src/ads.js
+++ src/ads.js
@@ -57,60 +57,22 @@ define(['q', 'react', 'underscore', 'src/layout' ], function(Q, React, _, layout
             });
         },
         render: function() {
-
-          var y = window.scrollY,
-              header = document.getElementById("header"),
-              header_height = header ? header.offsetHeight : 0,
-              footer = document.getElementById("footer-container"),
-              footer_height = footer ? footer.offsetHeight : 0,
-              top_el = document.getElementById("ads-banner-top") || {},
-              top_height = top_el.offsetHeight ? top_el.offsetHeight : 0,
-              top_width = top_el.offsetWidth ? top_el.offsetWidth : 0,
-              content = document.getElementById("content-container"),
-              height = content ? content.offsetHeight : 0,
-              bottom_el = document.getElementById("ads-banner-bottom") || {},
-              bottom_height = bottom_el.offsetHeight ? bottom_el.offsetHeight : 0,
-              bottom_width = bottom_el.offsetWidth ? bottom_el.offsetWidth : 0,
-              box_bottom_el = document.getElementById("ads-box-bottom") || {},
-              box_bottom_height = box_bottom_el.offsetHeight ? box_bottom_el.offsetHeight : 0,
-              box_bottom_width = box_bottom_el.offsetWidth ? box_bottom_el.offsetWidth : 0,
-              box_top_el = document.getElementById("ads-box-top") || {},
-              box_top_height = box_top_el.offsetHeight ? box_top_el.offsetHeight : 0,
-              box_top_width = box_top_el.offsetWidth ? box_top_el.offsetWidth : 0,
-              total_width = document.body.offsetWidth || 0,
-              total_width_padding = total_width - top_width,
-              total_width_padding_bottom = total_width - bottom_width,
-              bottom_width_base = Math.floor(total_width_padding_bottom/2),
-              top_width_base = Math.floor(total_width_padding/2),
-              box_top = (header_height + padding.top + top_height + padding.bottom + padding.top),
-              sidebar = padding.top + box_top_height + padding.bottom + padding.top + box_bottom_height + padding.bottom,
-              content_top = header_height + padding.top + top_height + padding.bottom,
-              total = header_height + padding.top + top_height + padding.bottom + height + padding.top + bottom_height + padding.bottom + footer_height;
-
-          if (sidebar > height) {
-            height = sidebar;
-          }
-          if (y > total) {
-            y = total;
-          } else if (y < 0) {
-            y = 0;
-          }
-
+          var current = layout.current();
           return React.DOM.section({
             id: "ads"
           }, React.DOM.section({
             id: "ads-banner-top",
             style: {
-              "left": top_width_base + "px",
-              "top": (header_height + padding.top) + "px"
+              "left": current.top_width_base + "px",
+              "top": (current.header_height + padding.top) + "px"
             }
           }, React.DOM.div({
             id: "ads-banner-top-ad"
           }) ), React.DOM.section({
             id: "ads-banner-bottom",
             style: {
-              "top": (content_top + height + padding.bottom) + "px",
-              "left": bottom_width_base + "px",
+              "top": (current.content_top + current.height + padding.bottom) + "px",
+              "left": current.bottom_width_base + "px",
             }
           }, React.DOM.div({
             id: "ads-banner-bottom-ad"
@@ -118,7 +80,7 @@ define(['q', 'react', 'underscore', 'src/layout' ], function(Q, React, _, layout
             id: "ads-box-header",
             style: {
               right: "50px",
-              top: ( y + 5 ) + "px"
+              top: ( current.y + 5 ) + "px"
             }
           }, React.DOM.div({
             id: "ads-box-header-ad"
@@ -126,7 +88,7 @@ define(['q', 'react', 'underscore', 'src/layout' ], function(Q, React, _, layout
             id: "ads-box-top",
             style: {
               "right": padding.right + "px",
-              "top": box_top + "px"
+              "top": current.box_top + "px"
             }
           }, React.DOM.div({
             id: "ads-box-top-ad"
@@ -134,10 +96,26 @@ define(['q', 'react', 'underscore', 'src/layout' ], function(Q, React, _, layout
             id: "ads-box-bottom",
             style: {
               "right": padding.right + "px",
-              "top": (box_top + box_top_height + padding.bottom + padding.top) + "px" //stream TK
+              "top": (current.box_top + current.box_top_height + padding.bottom + padding.top) + "px" //stream TK
             }
           }, React.DOM.div({
             id: "ads-box-bottom-ad"
+          }), React.DOM.section({
+            id: "ads-box-top-left",
+            style: {
+              "left": padding.left + "px",
+              "top": current.box_top + "px"
+            }
+          }, React.DOM.div({
+            id: "ads-box-top-ad-left"
+          }) ), React.DOM.section({
+            id: "ads-box-bottom-left",
+            style: {
+              "left": padding.left + "px",
+              "top": (current.box_top + current.box_top_height + padding.bottom + padding.top) + "px" //stream TK
+            }
+          }, React.DOM.div({
+            id: "ads-box-bottom-ad-left"
           }) ) );
         }
       });
@@ -163,7 +141,9 @@ define(['q', 'react', 'underscore', 'src/layout' ], function(Q, React, _, layout
     var top_banner,
         bottom_banner,
         top_box,
-        bottom_box;
+        bottom_box,
+        top_box_left,
+        bottom_box_left;
 
     googletag.pubads().enableSingleRequest();
     googletag.pubads().disableInitialLoad();
@@ -190,6 +170,16 @@ define(['q', 'react', 'underscore', 'src/layout' ], function(Q, React, _, layout
     bottom_box = googletag.defineSlot('/270461283/Box', [[88, 31]], "ads-box-bottom-ad")
         .addService(googletag.pubads());
     slots.push(bottom_box);
+    bottom_box.defineSizeMapping(boxmapping);
+
+    top_box_left = googletag.defineSlot('/270461283/Box', [[88, 31]], "ads-box-top-ad-left")
+        .addService(googletag.pubads());
+    top_box_left.defineSizeMapping(boxmapping);
+    slots.push(top_box_left);
+
+    bottom_box_left = googletag.defineSlot('/270461283/Box', [[88, 31]], "ads-box-bottom-ad-left")
+        .addService(googletag.pubads());
+    slots.push(bottom_box_left);
     bottom_box.defineSizeMapping(boxmapping);
 
     expecting = slots.length;
