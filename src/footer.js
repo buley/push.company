5a25e570b84b80d5fef366954870b5a6a8e14f25
Taylor Buley <taylor.buley@gmail.com>
2014-09-26 20:41:58 -0700

    .

--- src/footer.js
+++ src/footer.js
@@ -25,32 +25,11 @@ define(['q', 'react', 'underscore', 'src/layout'], function(Q, React, _, layout)
             instance = this;
         },
         render: function() {
-          var content = document.getElementById("content-container"),
-              height = content ? content.offsetHeight : 0,
-              header = document.getElementById("header"),
-              header_height = header ? header.offsetHeight : 0,
-              box_bottom_el = document.getElementById("ads-box-bottom") || {},
-              box_bottom_height = box_bottom_el.offsetHeight ? box_bottom_el.offsetHeight : 0,
-              box_bottom_width = box_bottom_el.offsetWidth ? box_bottom_el.offsetWidth : 0,
-              box_top_el = document.getElementById("ads-box-top") || {},
-              box_top_height = box_top_el.offsetHeight ? box_top_el.offsetHeight : 0,
-              box_top_width = box_top_el.offsetWidth ? box_top_el.offsetWidth : 0,
-              top_el = document.getElementById("ads-banner-top") || {},
-              top_height = top_el.offsetHeight ? top_el.offsetHeight : 0,
-              top_width = top_el.offsetWidth ? top_el.offsetWidth : 0,
-              bottom_el = document.getElementById("ads-banner-bottom") || {},
-              bottom_height = bottom_el.offsetHeight ? bottom_el.offsetHeight : 0,
-              bottom_width = bottom_el.offsetWidth ? bottom_el.offsetWidth : 0,
-              right = padding.top + box_top_height + padding.bottom + padding.top + box_bottom_height + padding.bottom,
-              content_top = header_height + top_height;
-
-          if (right > height) {
-            height = right;
-          }
+          var current = layout.current();
           return React.DOM.section({
             id: "footer-container",
             style: {
-              "top": (content_top + height + bottom_height + 80) + "px",
+              "top": (current.content_top + current.content_height + current.bottom_height + (2 * padding.top) + (2 * padding.bottom)) + "px",
             }
           }, React.DOM.section({
             id: "footer"
