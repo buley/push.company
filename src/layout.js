615899fbe67ce146ff7f82b295ac498553f6f588
Taylor Buley <taylor.buley@gmail.com>
2014-09-28 22:15:58 -0700

    .

--- src/layout.js
+++ src/layout.js
@@ -166,7 +166,7 @@ define([], function() {
       console.log('YEAH',left_sidebar, height);
       console.log('um',left_sidebar + bottom_left_height + top_left_height);
 
-      if (left_sidebar > height) {
+      if ((left_sidebar + bottom_left_height + top_left_height) > height) {
         height = left_sidebar + bottom_left_height + top_left_height;
       }
 
