a05b5dd2e2bf4aba99e6933ba9f4c98f2f147c0a
Taylor Buley <taylor.buley@gmail.com>
2014-09-29 09:28:42 -0700

    .

--- src/layout.js
+++ src/layout.js
@@ -146,7 +146,7 @@ define([], function() {
       /* Middle */
 
       middle_right = sidebar - 100 - box_top_height - box_bottom_height - top_right_height;
-      middle_left = left_sidebar - bottom_left_height - 140 - left_box_top_height - left_box_bottom_height - top_left_height;
+      middle_left = left_sidebar - bottom_left_height - 160 - left_box_top_height - left_box_bottom_height - top_left_height;
 
       min_diff_r = 0;
       min_diff_l = 0;
