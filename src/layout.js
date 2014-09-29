a869bc28ee7d8120142c9c4fe527123b4089dc22
Taylor Buley <taylor.buley@gmail.com>
2014-09-28 21:15:42 -0700

    .

--- src/layout.js
+++ src/layout.js
@@ -9,10 +9,16 @@ define([], function() {
           },
           middle_left,
           middle_right,
+          top_left,
+          bottom_left,
+          top_right,
+          bottom_right,
           min_width = 960,
           min_middle = 420,
           min_diff_r = 0,
           min_diff_l = 0,
+          min_top = 200,
+          min_bottom = 200,
           y = window.scrollY,
           total_width = document.body.offsetWidth || 0,
           has_ads = true,
@@ -50,10 +56,23 @@ define([], function() {
           right_el = document.getElementById("right-container") || {},
           right_height = right_el.offsetHeight ? right_el.offsetHeight : 0,
           right_width = right_el.offsetWidth ? right_el.offsetWidth : 0,
+          top_left_el = document.getElementById("top-left-container"),
+          top_left_height = top_left_el ? top_left_el.offsetHeight : 0,
+          top_left_width = top_left_el ? top_left_el.offsetHeight : 0,
+          bottom_left_el = document.getElementById("bottom-left-container"),
+          bottom_left_height = bottom_left_el ? bottom_left_el.offsetHeight : 0,
+          bottom_left_width = bottom_left_el ? bottom_left_el.offsetHeight : 0,
+          top_right_el = document.getElementById("top-right-container"),
+          top_right_height = top_right_el ? top_right_el.offsetHeight : 0,
+          top_right_width = top_right_el ? top_right_el.offsetHeight : 0,
+          bottom_right_el = document.getElementById("bottom-right-container"),
+          bottom_right_height = bottom_right_el ? bottom_right_el.offsetHeight : 0,
+          bottom_right_width = bottom_right_el ? bottom_right_el.offsetHeight : 0,
           total_width_padding = total_width - top_width,
           total_width_padding_bottom = total_width - bottom_width,
           bottom_width_base = Math.floor(total_width_padding_bottom/2),
           top_width_base = Math.floor(total_width_padding/2),
+          content_top = (header_height + padding.top + (top_height > 0 ? padding.top + top_height + padding.bottom : 0 ) ),
           box_top = (header_height + padding.top + (top_height > 0 ? padding.top + top_height + padding.bottom : 0 ) ),
           sidebar = padding.top + box_top_height + padding.bottom + padding.top + box_bottom_height + padding.bottom,
           left_sidebar = padding.top + left_box_top_height + padding.bottom + padding.top + left_box_bottom_height + padding.bottom,
@@ -84,6 +103,20 @@ define([], function() {
         width = total_width - right_width - left_width;
       }
 
+      /* Top */
+
+      if (top_right_height < min_top) {
+        top_right_height = min_top;
+      }
+      box_top += top_right_height + 20;
+
+      if (top_left_height < min_top) {
+        top_left_height = min_top;
+      }
+      left_box_top += top_left_height + 20;
+
+      /* Middle */
+
       middle_right = sidebar - 120 - box_top_height - box_bottom_height;
       middle_left = left_sidebar - 120 - left_box_top_height - left_box_bottom_height;
 
@@ -99,6 +132,16 @@ define([], function() {
 
       middle_left += min_diff_l;
       left_sidebar += min_diff_l;
+
+      /* Bottom */
+
+      if (bottom_right_height < min_bottom) {
+        bottom_right_height = min_bottom;
+      }
+
+      if (bottom_left_height < min_bottom) {
+        bottom_left_height = min_bottom;
+      }
 
       if (sidebar > height) {
         height = sidebar;
@@ -145,6 +188,7 @@ define([], function() {
         bottom_width_base: bottom_width_base,
         top_width_base: top_width_base,
         box_top: box_top,
+        content_top: content_top,
         sidebar: sidebar,
         left_sidebar: left_sidebar,
         content_top: content_top,
@@ -154,7 +198,15 @@ define([], function() {
         right_width: right_width,
         left_height: left_height,
         middle_left: middle_left,
-        middle_right: middle_right
+        middle_right: middle_right,
+        top_right_height: top_right_height,
+        top_left_height: top_left_height,
+        top_right_width: top_right_width,
+        top_left_width: top_left_width,
+        bottom_right_height: top_right_height,
+        bottom_left_height: top_left_height,
+        bottom_right_width: top_right_width,
+        bottom_left_width: top_left_width
       };
     }
   }
