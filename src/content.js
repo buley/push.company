445c7735a376aec4bf2d244574aca4b66d1fb262
Taylor Buley <taylor.buley@gmail.com>
2014-09-28 23:55:56 -0700

    .

--- src/content.js
+++ src/content.js
@@ -24,7 +24,14 @@ define(['q', 'react', 'underscore', 'src/layout'], function(Q, React, _, layout)
             React.DOM.div({id: "content-header-title"}, React.DOM.h1({}, "Biltong tail spare ribs turkey prosciutto hamburger pig sausage")),
             React.DOM.div({id: "content-header-description"}, React.DOM.span({}, "Bacon ipsum dolor sit amet capicola shoulder meatball, bacon tenderloin salami ham short loin frankfurter rump.")),
             React.DOM.div({id: "content-header-byline"}, React.DOM.span({}, "Andouille Meatloaf"), React.DOM.span({}, ", Staff Writer"))
-          ), React.DOM.section({id: "content-body"}, React.DOM.p({}, "Blah") ) ) );
+          ),
+            React.DOM.section({id: "content-body"},
+              React.DOM.p({}, "Venison pork pork chop frankfurter tongue tail ball tip. Leberkas pancetta hamburger ground round brisket tenderloin."),
+              React.DOM.p({}, "Strip steak pig drumstick bacon. Boudin chicken jowl short loin, filet mignon swine capicola pancetta ham hock."),
+              React.DOM.p({}, "Ribeye capicola shankle frankfurter turducken salami strip steak kevin rump venison biltong meatloaf porchetta short loin. Capicola ground round sirloin filet mignon tail shank meatball kielbasa kevin sausage hamburger boudin cow."),
+              React.DOM.p({}, "Brisket ham hock jowl flank shankle corned beef.")
+            )
+          ) );
         }
       });
 
