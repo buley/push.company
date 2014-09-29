50a5c5e8158a10f361282bda6b63ae2287d94baf
Taylor Buley <taylor.buley@gmail.com>
2014-09-28 21:50:38 -0700

    .

--- src/layout.js
+++ src/layout.js
@@ -123,8 +123,8 @@ define([], function() {
       //left_box_top = box_top + top_left_height;
       //box_top += top_right_height;
 
-      sidebar += top_right_height;
-      left_sidebar += top_left_height;
+      //sidebar += top_right_height;
+      //left_sidebar += top_left_height;
 
       /* Middle */
 
