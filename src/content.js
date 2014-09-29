0007e01197204254fdfd9cebc02c3ca167d33d83
Taylor Buley <taylor.buley@gmail.com>
2014-09-29 13:49:41 -0700

    .

--- src/content.js
+++ src/content.js
@@ -22,8 +22,8 @@ define(['q', 'react', 'underscore', 'src/layout'], function(Q, React, _, layout)
           }, React.DOM.section({ id: "content-header"},
             React.DOM.div({id: "content-header-pubdate"},
               React.DOM.span({}, "August 20, 2014"),
-                React.DOM.span({id: "content-header-tools", style: {float: "right"}},
-                React.DOM.span({id: "content-header-tools-map", style: {float: "right"}}, React.DOM.img({src:"/img/tab_black_map@2x.png"}))
+                React.DOM.div({id: "content-header-tools", style: {float: "right"}},
+                React.DOM.div({id: "content-header-tools-map", style: {float: "right"}}, React.DOM.img({src:"/img/tab_black_map@2x.png"}))
               )
             ),
             React.DOM.div({id: "content-header-title"}, React.DOM.h1({}, "Biltong tail spare ribs turkey prosciutto hamburger pig sausage")),
