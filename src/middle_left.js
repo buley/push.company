289ac1404dd9731d4c3b4b5b4cf169a1259ff39e
Taylor Buley <taylor.buley@gmail.com>
2014-09-28 20:12:55 -0700

    .

--- src/middle_left.js
+++ src/middle_left.js
@@ -8,8 +8,8 @@ define(['q', 'react', 'underscore', 'src/layout'], function(Q, React, _, layout)
           return React.DOM.section({
             id: "middle-left-container",
             style: {
-              top: (current.box_top + 40 + current.left_box_top_height) + "px",
-              height: current.middle_left + "px",
+              top: (current.box_top + 20 + current.left_box_top_height) + "px",
+              height: (current.middle_left + 40) + "px",
               left: current.padding.left,
               display: current.has_left ? "inline" : "none"
             }
