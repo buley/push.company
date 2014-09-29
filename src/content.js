f5a4f3899e07d001eb1344becb63a7e9a588ef8a
Taylor Buley <taylor.buley@gmail.com>
2014-09-28 23:26:02 -0700

    .

--- src/content.js
+++ src/content.js
@@ -21,9 +21,7 @@ define(['q', 'react', 'underscore', 'src/layout'], function(Q, React, _, layout)
             }
           }, React.DOM.section({ id: "content-header"},
             React.DOM.div({id: "content-header-title"}, React.DOM.h1({}, "Biltong tail spare ribs turkey prosciutto hamburger pig sausage")),
-            React.DOM.div({id: "content-header-description"}, React.DOM.div( {}, "Bacon ipsum dolor sit amet capicola shoulder meatball, bacon tenderloin salami ham short loin frankfurter rump venison porchetta landjaeger. Leberkas chuck pork loin pork belly venison. Meatloaf filet mignon pork belly doner ribeye landjaeger meatball salami strip steak pork sausage tri-tip turkey. Andouille pork chop ball tip tail jowl, shank kevin ham.
-
-")),
+            React.DOM.div({id: "content-header-description"}, React.DOM.div({}, "Bacon ipsum dolor sit amet capicola shoulder meatball, bacon tenderloin salami ham short loin frankfurter rump venison porchetta landjaeger. Leberkas chuck pork loin pork belly venison. Meatloaf filet mignon pork belly doner ribeye landjaeger meatball salami strip steak pork sausage tri-tip turkey. Andouille pork chop ball tip tail jowl, shank kevin ham.")),
             React.DOM.div({id: "content-header-pubdate"}),
             React.DOM.div({id: "content-header-byline"}, React.DOM.div({}, "Andouille Meatloaf"))
           ) ) );
