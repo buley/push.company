524871b4c5683d4edb4a054d0ac5987fda857535
Taylor Buley <taylor.buley@gmail.com>
2014-09-29 00:30:08 -0700

    .

--- src/content.js
+++ src/content.js
@@ -20,7 +20,7 @@ define(['q', 'react', 'underscore', 'src/layout'], function(Q, React, _, layout)
               "height": current.height + "px"
             }
           }, React.DOM.section({ id: "content-header"},
-            React.DOM.div({id: "content-header-pubdate"}, React.DOM.span({}, "August 20, 2014"), React.DOM.span({}, "Map")),
+            React.DOM.div({id: "content-header-pubdate"}, React.DOM.span({}, "August 20, 2014"), React.DOM.span({style: {float: "right"}}, "Map")),
             React.DOM.div({id: "content-header-title"}, React.DOM.h1({}, "Biltong tail spare ribs turkey prosciutto hamburger pig sausage")),
             React.DOM.div({id: "content-header-description"}, React.DOM.span({}, "Bacon ipsum dolor sit amet capicola shoulder meatball, bacon tenderloin salami ham short loin frankfurter rump.")),
             React.DOM.div({id: "content-header-byline"}, React.DOM.span({}, "Andouille Meatloaf"), React.DOM.span({}, ", Staff Writer"))
